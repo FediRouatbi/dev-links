f01cce44ee7fc8645167da371adb03e3c45de4a9
Fedi Rouatbi <fedirouatbi@gmail.com>
2023-09-16 16:20:50 +0100

    add links placeholder and remove functionality

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,24 +1,39 @@
 import { Select } from "@/components";
+import { Icons } from "@/constants";
+import { useRbk } from "@/context/Provider";
 import { Link as LinkIcon, AlignJustify } from "lucide-react";
-const Link = () => {
+const Link = ({
+  link,
+  platform,
+  id,
+  num,
+}: {
+  link: string;
+  platform: string;
+  id: string;
+  num: number;
+}) => {
+  const { deleteLink } = useRbk();
   return (
-    <div className="bg-gray-100 rounded-lg p-4">
-      <div className=" flex justify-between mb-5">
+    <div className="p-4 bg-gray-100 rounded-lg">
+      <div className="flex justify-between mb-5 ">
         <div className="flex items-center gap-2">
           <button>
             <AlignJustify />
           </button>
           <label
             htmlFor=""
-            className="block  text-sm font-medium text-gray-900 "
+            className="block text-sm font-medium text-gray-900 "
           >
-            Link #1
+            Link #{num}
           </label>
         </div>
 
-        <button>remove</button>
+        <button onClick={() => deleteLink(id)} className="hover:text-red-300">
+          remove
+        </button>
       </div>
-      <Select />
+      <Select disabled platform={platform as Icons} />
 
       <div className="mt-4">
         <label
@@ -29,15 +44,15 @@ const Link = () => {
         </label>
         <div className="relative">
           <LinkIcon
-            className="absolute top-1/2 left-4 -translate-y-1/2"
+            className="absolute -translate-y-1/2 top-1/2 left-4"
             size={20}
           />
           <input
             type="text"
             id="first_name"
             className=" pl-11 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-            placeholder="url"
-            required
+            placeholder={link}
+            disabled
           />
         </div>
       </div>
