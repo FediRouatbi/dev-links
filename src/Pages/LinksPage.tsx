a7bbf834e3f46ec4b11ec914d056f953f2a3f50b
Fedi Rouatbi <fedirouatbi@gmail.com>
2023-09-16 16:26:29 +0100

    fix navigation usiing reac trouter outlet

--- src/Pages/LinksPage.tsx
+++ src/Pages/LinksPage.tsx
@@ -1,11 +1,22 @@
-import { Form, MobileView, Profile } from "@/components";
+import { MobileView, Navbar } from "@/components";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 const LinksPage = () => {
+  const { pathname } = useLocation();
+
+  if (pathname === "/") {
+    return <Navigate replace to="/links" />;
+  }
+
   return (
-    <section className="grid grid-cols-[2fr,4fr] gap-5  py-10">
-      <MobileView />
-      <Form />
-      {/* <Profile /> */}
+    <section className="p-4 mx-auto max-w-7xl">
+      <Navbar />
+      <section className="grid grid-cols-1   sm:grid-cols-[3fr,4fr] gap-5  py-10 min-h-[800px]">
+        <div className="relative block bg-white rounded-lg min-h-[700px]">
+          <MobileView />
+        </div>
+        <Outlet />
+      </section>
     </section>
   );
 };
