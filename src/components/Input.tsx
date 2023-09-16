b07cc91f40e49d485ef9cbcada17e17bf7ebf15f
Fedi Rouatbi <fedirouatbi@gmail.com>
2023-09-16 16:22:11 +0100

    make input more customizable

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,27 +1,35 @@
-const Input = ({
-  placeHolder,
-  label,
-  required,
-}: {
-  placeHolder: string;
-  label: string;
-  required?: boolean;
-}) => {
+import { forwardRef } from "react";
+
+const Input = forwardRef<
+  HTMLInputElement,
+  {
+    placeHolder: string;
+    label: string;
+    required?: boolean;
+    error?: string;
+  }
+>(({ placeHolder, label, required, error, ...rest }, ref) => {
   return (
-    <div className=" relative  flex items-center justify-between w-full">
-      <label htmlFor="required-email" className="text-gray-700 w-1/3 ">
+    <div className="relative flex items-start justify-between w-full ">
+      <label htmlFor="required-email" className="w-1/3 text-gray-700 ">
         {label}
         {required && <span className="text-red-500 required-dot">*</span>}
       </label>
-      <input
-        type="text"
-        id="required-email"
-        className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-violet-600 focus:border-transparent"
-        name="email"
-        placeholder={placeHolder}
-      />
+      <div className="flex-1 w-full">
+        <input
+          ref={ref}
+          type="text"
+          id="required-email"
+          className="w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-transparent border-gray-300 rounded-lg shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-violet-600 focus:border-transparent"
+          name="email"
+          placeholder={placeHolder}
+          {...rest}
+        />
+
+        <p className="h-4 text-red-300 ">{error && error}</p>
+      </div>
     </div>
   );
-};
+});
 
 export { Input };
